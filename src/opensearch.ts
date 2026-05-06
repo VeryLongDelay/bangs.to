6c8a208b46a2621b2c330fe5afd9f7c791b011f5
Bill Xiong <billxiong99@gmail.com>
2026-05-05 23:12:55 -0700

    update opensearch xml

--- src/opensearch.ts
+++ src/opensearch.ts
@@ -5,11 +5,11 @@ export function opensearch(origin: string): Response {
     `<?xml version="1.0" encoding="UTF-8"?>
 <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
   <ShortName>${SITE_TITLE}</ShortName>
-  <Description>The lightning-fast, privacy-first duckduckgo-style bangs redirects</Description>
+  <Description>Lightning-fast, privacy-first DuckDuckGo-style bang redirects.</Description>
   <InputEncoding>UTF-8</InputEncoding>
   <Image width="16" height="16" type="image/svg+xml">${origin}/icon.svg</Image>
-  <Url type="text/html" template="${origin}/?q={searchTerms}"/>
-  <Url type="application/x-suggestions+json" template="${origin}/suggest?q={searchTerms}"/>
+  <Url type="text/html" method="get" template="${origin}/?q={searchTerms}"/>
+  <Url type="application/x-suggestions+json" template="https://duckduckgo.com/ac/?q=%s&type=list"/>
 </OpenSearchDescription>`,
     {
       headers: { 'Content-Type': 'application/opensearchdescription+xml' }
